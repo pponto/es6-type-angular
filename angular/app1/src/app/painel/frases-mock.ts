b298d3c92670c6caf05edf0aa9982c52a61a44e3
Pedro Moreira <pedromoreira@gmail.com>
2018-04-18 16:23:16 -0300

    add frases-mock to painel folder

---
+++ angular/app1/src/app/painel/frases-mock.ts
@@ -0,0 +1,8 @@
+import { Frase } from '../shared/frase.model'
+
+const FRASES: Frase[] = [
+  { fraseEng: 'I like to learn', frasePtBr: 'Eu gosto de aprender' },
+  { fraseEng: 'I watch TV', frasePtBr: 'Eu assisto tv' },
+  { fraseEng: 'How are you?', frasePtBr: 'Como vai você?' },
+  { fraseEng: 'I eat breas', frasePtBr: 'Eu como pão' }
+]
