177258352ea9ea402aeaa30cf3ee666671a3946e
Pedro Moreira <pedromoreira@gmail.com>
2018-04-18 16:27:56 -0300

    import frases to component

--- angular/app1/src/app/painel/frases-mock.ts
+++ angular/app1/src/app/painel/frases-mock.ts
@@ -1,8 +1,8 @@
-import { Frase } from '../shared/frase.model'
+import { Frase } from '../shared/frase.model';
 
-const FRASES: Frase[] = [
+export const FRASES: Frase[] = [
   { fraseEng: 'I like to learn', frasePtBr: 'Eu gosto de aprender' },
   { fraseEng: 'I watch TV', frasePtBr: 'Eu assisto tv' },
   { fraseEng: 'How are you?', frasePtBr: 'Como vai você?' },
   { fraseEng: 'I eat breas', frasePtBr: 'Eu como pão' }
-]
+];
