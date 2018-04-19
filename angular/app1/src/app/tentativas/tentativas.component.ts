bcedab1635e9e1e84f50084989bbbd2f7e3fd461
Pedro Moreira <pedromoreira@gmail.com>
2018-04-19 16:57:22 -0300

    add diretriz onChanges

--- angular/app1/src/app/tentativas/tentativas.component.ts
+++ angular/app1/src/app/tentativas/tentativas.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, OnChanges, Input } from '@angular/core';
 
 import { Coracao } from '../shared/coracao.model';
 
@@ -7,7 +7,7 @@ import { Coracao } from '../shared/coracao.model';
   templateUrl: './tentativas.component.html',
   styleUrls: ['./tentativas.component.css']
 })
-export class TentativasComponent implements OnInit {
+export class TentativasComponent implements OnInit, OnChanges {
 
   @Input() public tentativas: number
 
@@ -19,8 +19,13 @@ export class TentativasComponent implements OnInit {
     console.log(this.coracoes);
   }
 
-  ngOnInit() {
+  // ngOnChanges sempre será disparado após o @Input ser chamado
+  ngOnChanges() {
     console.log('tentativas recebidas do painel: ', this.tentativas);
+  }
+
+  ngOnInit() {
+
   }
 
 }
